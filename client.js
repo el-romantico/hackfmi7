dcf0f7ec596c271975fbc0735c0942fd85e88896
Stanislav Gatev <mail@s2gatev.com>
2016-05-28 00:56:17 +0300

    Add image label to canvas

--- client.js
+++ client.js
@@ -4,25 +4,33 @@ var snapshotContext = snapshot.getContext("2d");
 var viewport = document.querySelector("#viewport");
 
 function handleVideo(stream) {
-		viewport.src = window.URL.createObjectURL(stream);
+  viewport.src = window.URL.createObjectURL(stream);
 }
 
 function snapIt(event) {
-		snapshotContext.drawImage(viewport, 0, 0, snapshot.width, snapshot.height);
+  snapshotContext.drawImage(viewport, 0, 0, snapshot.width, snapshot.height);
 
-		var image = snapshot.toDataURL();
-		console.log(image);
+  var messages = new Array();
+  messages[0] = {time: 0, text: "PERSON", x: 90, y: 200};
+  messages.forEach(function(message) {
+    snapshotContext.font = "22px Lato";
+    snapshotContext.fillStyle = "#000000";
+    snapshotContext.fillText(message.text, message.x, message.y);
+  });
 
-		var request = new XMLHttpRequest();
-		request.onreadystatechange = function() {
-				if (request.readyState == 4 && request.status == 200) {
-						var myArr = JSON.parse(request.responseText);
-						console.log(myArr.results[0].tags[0].tag);
-				}
-		};
-		request.open("GET", "http://127.0.0.1:3000/analyze", true);
-		request.setRequestHeader('Accept', 'application/json');
-		request.send();
+  var image = snapshot.toDataURL();
+  console.log(image);
+
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      var myArr = JSON.parse(request.responseText);
+      console.log(myArr.results[0].tags[0].tag);
+    }
+  };
+  request.open("GET", "http://127.0.0.1:3000/analyze", true);
+  request.setRequestHeader('Accept', 'application/json');
+  request.send();
 }
 
-navigator.webkitGetUserMedia({video: true}, handleVideo, function(){});
+navigator.webkitGetUserMedia({video: true}, handleVideo, function() {});
