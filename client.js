ee0312a2d0d908feb93bf327ba308f417580e411
Stanislav Gatev <mail@s2gatev.com>
2016-05-28 03:17:34 +0300

    Add caption movement tracker

--- client.js
+++ client.js
@@ -1,39 +1,132 @@
+/**
+ * Video and canvas elements.
+ */
 var viewport = document.getElementById("viewport");
-
 var snapshot = document.getElementById("snapshot");
 var snapshotContext = snapshot.getContext("2d");
-
 var overlay = document.getElementById("overlay");
 var overlayContext = overlay.getContext("2d");
 
-function handleVideo(stream) {
-  viewport.src = window.URL.createObjectURL(stream);
+/**
+ * General purpose variables.
+ */
+var height = viewport.height;
+var width = viewport.width;
+
+/**
+ * Label movement configuration.
+ */
+var maxCaptionsCount = 100;
+var prevCaptionCoordinates = new Float32Array(maxCaptionsCount * 2);
+var captionCoordinates = new Float32Array(maxCaptionsCount * 2);
+var captionStatuses = new Uint8Array(maxCaptionsCount);
+var captionsCount = 0;
+var captionLabels = [];
+var imagePyramid = new jsfeat.pyramid_t(3);
+var prevImagePyramid = new jsfeat.pyramid_t(3);
+imagePyramid.allocate(width, height, jsfeat.U8_t | jsfeat.C1_t);
+prevImagePyramid.allocate(width, height, jsfeat.U8_t | jsfeat.C1_t);
+var winSize = 20;
+var maxIterations = 30;
+var epsilon = 0.01;
+var minEigen = 0.001;
+
+function createCaption(text, x, y) {
+  overlayContext.font = "22px Lato";
+  overlayContext.fillStyle = "#000000";
+  overlayContext.fillText(text, x, y);
 }
 
+// Extracts a still image from the video stream.
 function snapIt(event) {
-		snapshotContext.drawImage(viewport, 0, 0, snapshot.width, snapshot.height);
+  snapshotContext.drawImage(viewport, 0, 0, snapshot.width, snapshot.height);
 
-    overlayContext.clearRect(0, 0, overlay.width, overlay.height);
-    var messages = new Array();
-    messages[0] = {time: 0, text: "PERSON", x: 90, y: 200};
-    messages.forEach(function(message) {
-      overlayContext.font = "22px Lato";
-      overlayContext.fillStyle = "#000000";
-      overlayContext.fillText(message.text, message.x, message.y);
-    });
+  prevCaptionCoordinates = new Float32Array(maxCaptionsCount * 2);
+  captionCoordinates = new Float32Array(maxCaptionsCount * 2);
+  captionStatuses = new Uint8Array(maxCaptionsCount);
+  captionsCount = 0;
+  captionLabels = [];
 
-		var image = snapshot.toDataURL();
+  var messages = new Array();
+  messages[0] = {text: "PERSON", x: 90, y: 200};
+  messages[1] = {text: "BANANA", x: 390, y: 200};
+  messages.forEach(function(message) {
+    createCaption(message.text, message.x, message.y);
 
-		var request = new XMLHttpRequest();
-		request.onreadystatechange = function() {
-				if (request.readyState == 4 && request.status == 200) {
-						var myArr = JSON.parse(request.responseText);
-						console.log(myArr.results[0].tags[0].tag);
-				}
-		};
-		request.open("POST", "http://127.0.0.1:3000/analyze", true);
-		request.setRequestHeader('Accept', 'application/json');
-		request.send(image);
+    captionLabels[captionsCount] = message.text;
+    captionCoordinates[captionsCount << 1] = message.x;
+    captionCoordinates[(captionsCount << 1) + 1] = message.y;
+    captionsCount++;
+  });
+
+  var image = snapshot.toDataURL();
+
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      var myArr = JSON.parse(request.responseText);
+      console.log(myArr.results[0].tags[0].tag);
+    }
+  };
+  request.open("POST", "http://127.0.0.1:3000/analyze", true);
+  request.setRequestHeader('Accept', 'application/json');
+  request.send(image);
 }
 
-navigator.webkitGetUserMedia({video: true}, handleVideo, function() {});
+/**
+ * Add camera stream to viewport.
+ */
+navigator.webkitGetUserMedia(
+    {video: true},
+    function(stream) { viewport.src = window.URL.createObjectURL(stream); },
+    function() {});
+
+/**
+ * Follow caption movements.
+ */
+function tick() {
+  compatibility.requestAnimationFrame(tick);
+
+  if (viewport.readyState === viewport.HAVE_ENOUGH_DATA) {
+    overlayContext.drawImage(viewport, 0, 0, width, height);
+    var imageData = overlayContext.getImageData(0, 0, width, height);
+
+    var _pt_xy = prevCaptionCoordinates;
+    prevCaptionCoordinates = captionCoordinates;
+    captionCoordinates = _pt_xy;
+    var _pyr = prevImagePyramid;
+    prevImagePyramid = imagePyramid;
+    imagePyramid = _pyr;
+
+    jsfeat.imgproc.grayscale(
+        imageData.data, width, height, imagePyramid.data[0]);
+
+    imagePyramid.build(imagePyramid.data[0], true);
+
+    jsfeat.optical_flow_lk.track(
+        prevImagePyramid, imagePyramid, prevCaptionCoordinates,
+        captionCoordinates, captionsCount, winSize | 0, maxIterations | 0,
+        captionStatuses, epsilon, minEigen);
+
+    var updatedCaptionsCount = 0;
+
+    for (var i = 0; i < captionsCount; ++i) {
+      if (captionStatuses[i] == 1) {
+        if (updatedCaptionsCount < i) {
+          captionLabels[updatedCaptionsCount] = captionLabels[i];
+          captionCoordinates[updatedCaptionsCount << 1] =
+              captionCoordinates[i << 1];
+          captionCoordinates[(updatedCaptionsCount << 1) + 1] =
+              captionCoordinates[(i << 1) + 1];
+        }
+        createCaption(
+            captionLabels[updatedCaptionsCount],
+            captionCoordinates[updatedCaptionsCount << 1],
+            captionCoordinates[(updatedCaptionsCount << 1) + 1]);
+        ++updatedCaptionsCount;
+      }
+    }
+    captionsCount = updatedCaptionsCount;
+  }
+}
+compatibility.requestAnimationFrame(tick);
